ec2bb78fe5e69b1401ed61a633c9220bd5478ab6
nishayadav2000 <107976034+nishayadav2000@users.noreply.github.com>
2024-02-08 11:21:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+(function(){
+    const buttons = document.querySelectorAll(".btn-counter");
+    let count = 0;
+
+    const counter = document.querySelector(".counter-machine");
+    const counterContainer = document.querySelector(".counter");
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(){
+            if(button.classList.contains("decrease-btn")){
+                count--;
+            } else if(button.classList.contains("increase-btn")){
+                count++;
+            }
+            counter.textContent = count;
+
+            if(count > 0){
+                counterContainer.classList.add("positive");
+                counterContainer.classList.remove("negative");
+            } else if (count < 0){
+                counterContainer.classList.add("negative");
+                counterContainer.classList.remove("positive");
+            } else {
+                counterContainer.classList.remove("positive");
+                counterContainer.classList.remove("negative");
+            }
+        });
+    });
+})();
